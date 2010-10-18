88150fca8a69d9c16488f0fb87873db90fea2e07
nicohen <nicohen@f0eca99e-9158-631e-3d1b-0a23f373bf7a>
2010-10-18 15:45:08 +0000

    - DbAdapter.java para manejar SQLite - Se obtienen las piezas de sushi de la base de datos - Se modifica la OrderActivity para que extienda de ListActivity

--- HelloAndroid/src/com/nmc/HelloAndroid/OrderActivity.java
+++ HelloAndroid/src/com/nmc/HelloAndroid/OrderActivity.java
@@ -1,75 +1,68 @@
 package com.nmc.HelloAndroid;
 
-import android.app.Activity;
-import android.content.Context;
+import android.app.ListActivity;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
 
-public class OrderActivity extends Activity {
-	
-	private static class EfficientAdapter extends BaseAdapter {
-		private LayoutInflater mInflater;
-		private Resources resources;
+import com.nmc.HelloAndroid.adapter.DbAdapter;
+import com.nmc.HelloAndroid.adapter.OrderCursorAdapter;
 
-		public EfficientAdapter(Context context, Resources resources) {
-			mInflater = LayoutInflater.from(context);
-			this.resources = resources;
-		}
+public class OrderActivity extends ListActivity {
 
-		public int getCount() {
-			return products.length;
-		}
-
-		public Object getItem(int position) {
-			return position;
-		}
-
-		public long getItemId(int position) {
-			return position;
-		}
-
-		public View getView(int position, View convertView, ViewGroup parent) {
-			ViewHolder holder;
-			if (convertView == null) {
-				convertView = mInflater.inflate(R.layout.listview, null);
-				holder = new ViewHolder();
-				holder.image = (ImageView) convertView.findViewById(R.id.TextView01);
-				holder.description = (TextView) convertView.findViewById(R.id.TextView02);
-
-				convertView.setTag(holder);
-			} else {
-				holder = (ViewHolder) convertView.getTag();
-			}
-			holder.image.setBackgroundResource(products[position]);
-			holder.description.setText(resources.getResourceEntryName(products[position]));
-
-			return convertView;
-		}
-
-		static class ViewHolder {
-			ImageView image;
-			TextView description;
-		}
-	}
+	private DbAdapter mDbHelper;
+	private Cursor mCursor;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.order_layout);
-		ListView l1 = (ListView) findViewById(R.id.ListView01);
-		l1.setAdapter(new EfficientAdapter(this,getResources()));
+		mDbHelper = new DbAdapter(this);
+		mDbHelper.open();
+		initProducts(getResources());
+		fillData();
+		registerForContextMenu(getListView());
 	}
 
-	private static final int[] products = { R.drawable.buenosaires, R.drawable.california, 
-		R.drawable.crazy, R.drawable.feel, R.drawable.futurama, R.drawable.geishas, R.drawable.honey, 
-		R.drawable.hot, R.drawable.newyork, R.drawable.philadelphia, R.drawable.placerreal, R.drawable.salmonskin, 
-		R.drawable.sc, R.drawable.smoke, R.drawable.soul, R.drawable.spf, R.drawable.tamago, R.drawable.tiago};
+	private void initProducts(Resources resources) {
+		for (int i = 0; i < products.length; i++) {
+			mDbHelper.createProduct(
+					capitalize(resources.getResourceEntryName(products[i])), 1,
+					"");
+		}
+	}
+
+	private void fillData() {
+		// Get all of the rows from the database and create the item list
+		mCursor = mDbHelper.fetchAllProducts();
+		startManagingCursor(mCursor);
+
+		// Create an array to specify the fields we want to display in the list
+		// (TITLE)
+		String[] from = new String[] { DbAdapter.KEY_TITLE };
+
+		// and an array of the fields we want to bind those fields to (in this
+		// case just order_row)
+		int[] to = new int[] { R.id.order_title };
+
+		// Now create a simple cursor adapter and set it to display
+		OrderCursorAdapter notes = new OrderCursorAdapter(this,
+				R.layout.order_list_row, mCursor, from, to);
+		setListAdapter(notes);
+	}
+
+	public static String capitalize(String s) {
+		if (s.length() == 0)
+			return s;
+		return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+	}
+
+	private static final int[] products = { R.drawable.buenosaires,
+			R.drawable.california, R.drawable.crazy, R.drawable.feel,
+			R.drawable.futurama, R.drawable.geishas, R.drawable.honey,
+			R.drawable.hot, R.drawable.newyork, R.drawable.philadelphia,
+			R.drawable.placerreal, R.drawable.salmonskin, R.drawable.sc,
+			R.drawable.smoke, R.drawable.soul, R.drawable.spf,
+			R.drawable.tamago, R.drawable.tiago };
 
 }
